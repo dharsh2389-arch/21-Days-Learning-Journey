d83ee88a923042ab9c929f4eebd38e9abaff816a
Dharshini K <dharsh2389@gmail.com>
2026-02-17 21:29:42 +0530

    Update code.py

--- Day1/code.py
+++ Day1/code.py
@@ -1,1 +1,21 @@
+#Day 1 - Python Introduction
 
+#Printing text
+print("Hello World")
+print("I am Dharshini")
+print("Welcome to Python Learning")
+
+#Printing numbers
+print(5)
+print(1000)
+
+#Printing multiple lines
+print("Day 1")
+print("I have started my 21 day learning journey today with my friends and mates")
+
+#Simple star pattern
+print("*")
+print("**")
+print("***")
+print("****")
+print("*****")

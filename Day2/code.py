dd153ba8e63046b8a0bc7a3c6353c9328ff8877e
Dharshini K <dharsh2389@gmail.com>
2026-02-18 18:33:39 +0530

    Create code.py

---
+++ Day2/code.py
@@ -0,0 +1,20 @@
+#Creating variables
+
+name = "Dharshini"
+age = 19
+marks = 98
+is_student = True
+
+#Printing values
+
+print("Name: ",name)
+print("Age: ",age)
+print("Marks: ",marks)
+print("Student: ",is_student)
+
+#Checking data types
+
+print(type(name))
+print(type(age))
+print(type(marks))
+print(type(is_student))

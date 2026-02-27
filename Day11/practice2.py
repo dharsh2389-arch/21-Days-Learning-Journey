116f8a68eb5db68669823beea3b5420e35d56775
Dharshini K <dharsh2389@gmail.com>
2026-02-27 20:51:15 +0530

    Create practice2.py

---
+++ Day11/practice2.py
@@ -0,0 +1,14 @@
+numbers = [10, 20, 30, 40, 50]
+
+print("Original Array:", numbers)
+
+numbers.append(60)
+print("After Append:", numbers)
+
+numbers.remove(20)
+print("After Remove:", numbers)
+
+print("Length:", len(numbers))
+print("Maximum:", max(numbers))
+print("Minimum:", min(numbers))
+print("Sum:", sum(numbers))

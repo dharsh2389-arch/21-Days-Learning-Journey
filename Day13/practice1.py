ac5a55e5be74dbca42fba909be182c231ec09b5a
Dharshini K <dharsh2389@gmail.com>
2026-03-01 17:14:02 +0530

    Create practice1.py

---
+++ Day13/practice1.py
@@ -0,0 +1,15 @@
+'''Create a class called Laptop.
+Just define the class.
+Create 2 objects from it.
+Print the type of one object.'''
+
+class Laptop:
+    pass
+
+l1 = Laptop()
+l2 = Laptop()
+
+print(type(l1))
+
+'''Output:
+<class '__main__.Laptop'>'''

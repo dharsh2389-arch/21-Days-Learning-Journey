722b17fa065a4b00a6ceb316743a8e80712fbe6a
Dharshini K <dharsh2389@gmail.com>
2026-03-13 22:44:32 +0530

    Create number_pyramid_pattern.py

---
+++ Day25/number_pyramid_pattern.py
@@ -0,0 +1,14 @@
+n = int(input("Enter number: "))
+for i in range(1, n + 1):
+    for j in range(i):
+        print(i, end="")
+    print()
+
+
+'''Output
+Enter number: 5
+1
+22
+333
+4444
+55555'''

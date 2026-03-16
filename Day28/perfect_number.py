ae3ea7aafa0294f0c16401c47bae4075bef75b93
Dharshini K <dharsh2389@gmail.com>
2026-03-16 15:50:29 +0530

    Create perfect_number.py

---
+++ Day28/perfect_number.py
@@ -0,0 +1,15 @@
+num = int(input("Enter number: "))
+sum = 0
+for i in range(1, num):
+    if num % i == 0:
+        sum += i
+if sum == num:
+    print("Perfect number")
+else:
+    print("Not a perfect number")
+
+'''
+Ouput
+Enter number: 6
+Perfect number
+'''

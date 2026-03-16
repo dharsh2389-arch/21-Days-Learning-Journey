95a2e7aad63112e68936e0aff7c75de67079ecd9
Dharshini K <dharsh2389@gmail.com>
2026-03-16 15:54:02 +0530

    Create factorial.py

---
+++ Day28/factorial.py
@@ -0,0 +1,11 @@
+num = int(input("Enter number: "))
+factorial = 1
+for i in range(1, num + 1):
+    factorial *= i
+print("Factorial:", factorial)
+
+'''
+Output
+Enter number: 25
+Factorial: 15511210043330985984000000
+'''
